05d763c1eaff16ce7f5d7f2d0ccaa557b12a6cf4
raviyamdmini-stack <raviyamdmini@gmail.com>
2025-12-14 06:08:53 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,22 @@
+const BOT_NAME_FANCY = '𝐑𝐀𝐕𝐈𝐘𝐀-𝐌𝐃 𝐌𝐈𝐍𝐈 𝐁𝐎𝐓';
+
+const config = {
+  AUTO_VIEW_STATUS: 'true',
+  AUTO_LIKE_STATUS: 'true',
+  AUTO_RECORDING: 'false',
+  AUTO_LIKE_EMOJI: ['🔥','🌞','🌈','❤️','🌸','🦚','🐠','🐞'],
+  PREFIX: '.',
+  MAX_RETRIES: 3,
+  GROUP_INVITE_LINK: 'https://chat.whatsapp.com/Eqv2HOSQt1TGvB4NHtmDjL?mode=hqrt2',
+  RCD_IMAGE_PATH: 'https://i.ibb.co/gM997gRp/tourl-1765543281755.jpg',
+  NEWSLETTER_JID: '120363401853152721@newsletter',
+  OTP_EXPIRY: 300000,
+  OWNER_NUMBER: process.env.OWNER_NUMBER || '94785505762',
+  CHANNEL_LINK: 'https://whatsapp.com/channel/0029VbB8Sv72phHMubLEgH0F',
+  BOT_NAME: '𝐑𝐀𝐕𝐈𝐘𝐀 𝐌𝐃 𝐕1',
+  BOT_VERSION: '5.0.0V',
+  OWNER_NAME: 'PAMUDINA RAVIHARA ',
+  IMAGE_PATH: 'https://i.ibb.co/KxrG6Fpm/IMG-20251011-WA0084.jpg',
+  BOT_FOOTER: '𝐑𝐀𝐕𝐈𝐘𝐀-𝐌𝐃 𝐕1',
+  BUTTON_IMAGES: { ALIVE: 'https://i.ibb.co/gM997gRp/tourl-1765543281755.jpg'
+};
